3177cc8179335fdb4bea7c4eff66433bdcbe4536
Ryan Spreier <rspreier@gmail.com>
2024-03-15 15:11:23 -0700

    Added a bit of user interface to the calendar page

--- app/src/main/java/com/example/final_project_bryants/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/final_project_bryants/ui/calendar/CalendarFragment.kt
@@ -1,17 +1,16 @@
-package com.example.final_project_bryants.ui.slideshow
+package com.example.final_project_bryants.ui.calendar
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.final_project_bryants.databinding.FragmentSlideshowBinding
+import com.example.final_project_bryants.databinding.FragmentCalendarBinding
 
-class SlideshowFragment : Fragment() {
+class CalendarFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentCalendarBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,15 +22,11 @@ class SlideshowFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+            ViewModelProvider(this).get(CalendarViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- app/src/main/java/com/example/final_project_bryants/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/final_project_bryants/ui/calendar/CalendarViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.final_project_bryants.ui.slideshow
+package com.example.final_project_bryants.ui.calendar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class CalendarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
