fbcb87bb37f3157d6b4dfecabde8f73d8bb1a118
Ryan Spreier <rspreier@gmail.com>
2024-03-17 16:55:44 -0700

    Edited home page to have scrolling slide show of images

--- app/src/main/java/com/example/final_project_bryants/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/final_project_bryants/ui/home/HomeFragment.kt
@@ -4,39 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.final_project_bryants.databinding.FragmentHomeBinding
+import com.example.final_project_bryants.R
+import com.smarteist.autoimageslider.SliderView
+import com.smarteist.autoimageslider.SliderAnimations
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    lateinit var imageUrl: ArrayList<String>
+    lateinit var sliderView: SliderView
+    lateinit var sliderAdapter: SliderAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        sliderView = view.findViewById(R.id.slider)
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        imageUrl = ArrayList()
+        imageUrl.add("https://practice.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Fdsa-self-paced-thumbnail.png&w=1920&q=75")
+        imageUrl.add("https://practice.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Fdata-science-live-thumbnail.png&w=1920&q=75")
+        imageUrl.add("https://practice.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Ffull-stack-node-thumbnail.png&w=1920&q=75")
+
+        sliderAdapter = SliderAdapter(imageUrl)
+        // on below line we are setting auto cycle direction
+        // for our slider view from left to right.
+        sliderView.autoCycleDirection = SliderView.LAYOUT_DIRECTION_LTR
+        // on below line we are setting adapter for our slider.
+        sliderView.setSliderAdapter(sliderAdapter)
+
+        // on below line we are setting scroll time
+        // in seconds for our slider view.
+        sliderView.scrollTimeInSec = 5
+
+        // on below line we are setting auto cycle
+        // to true to auto slide our items.
+        sliderView.isAutoCycle = true
+
+        // on below line we are calling start
+        // auto cycle to start our cycle.
+        sliderView.startAutoCycle()
+
     }
 }
